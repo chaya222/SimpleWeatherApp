55edf3fe066c7fb7a8b5cbea2a88c7a95e3ca117
chaya222 <poonam.19923@gmail.com>
2020-02-10 20:52:30 +0530

    Added : all required dependencies in gradle Added : show weather forecast activity

---
+++ app/src/main/java/com/example/simpleweatherapp/feature/ShowWeatherForecastActivity.kt
@@ -0,0 +1,13 @@
+package com.example.simpleweatherapp.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simpleweatherapp.R
+
+class ShowWeatherForecastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_weather_forecast)
+    }
+}
